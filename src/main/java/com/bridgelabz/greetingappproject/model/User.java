ef22bd15cf7d14d530dfeac04e5d7a5deead6025
govind6498 <govindsingh6498@gmail.com>
2021-11-01 23:00:37 +0530

    Added:User bean or Data transfer pbject to store user details

---
+++ src/main/java/com/bridgelabz/greetingappproject/model/User.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.greetingappproject.model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void getLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
