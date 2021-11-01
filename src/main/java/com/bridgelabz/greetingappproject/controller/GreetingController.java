b7c0f291416202622a575f56cf0f8eb85c7a4e18
govind6498 <govindsingh6498@gmail.com>
2021-11-01 23:17:45 +0530

    MODIFIED:ToConvert response message from http methods to json

--- src/main/java/com/bridgelabz/greetingappproject/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingappproject/controller/GreetingController.java
@@ -11,27 +11,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.greetingappproject.model.Greeting;
 import com.bridgelabz.greetingappproject.model.User;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 	private static final String template = "Hello,%s!";
+	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping(value= {"","/","/home"})
-	public String sysHello() {
-		return String.format(template,"Wolrd");
+	public Greeting sysHello() {
+		return new Greeting(counter.incrementAndGet(),String.format(template,"World"));
 	}
 	@GetMapping("/query")
-	public String greeting(@RequestParam(value="name",defaultValue="World") String name) {
-		return String.format(template, name);
+	public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(),String.format(template,name));
 	}
 	@PostMapping("/post")
-	public String greeting(@RequestBody User user) {
-		return String.format(template, user.getFirstName()+" "+user.getLastName());
+	public Greeting greeting(@RequestBody User user) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, user.getFirstName()+" "+user.getLastName()));
 	}
 	@PutMapping("put/{firstName}")
-	public String greeting(@PathVariable String firstName,@RequestParam(value="lastName") String lastName){
-		return String.format(template,firstName+" "+lastName);
+	public Greeting greeting(@PathVariable String firstName,@RequestParam(value="lastName") String lastName){
+		return new Greeting(counter.incrementAndGet(),String.format(template,firstName+" "+lastName));
 	}
 }
