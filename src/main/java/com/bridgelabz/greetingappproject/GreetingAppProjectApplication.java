6796639008bc3f8d5ecb45e401a1acfcc0e789b0
govind6498 <govindsingh6498@gmail.com>
2021-11-01 22:01:05 +0530

    Added:To initialize project Structure

---
+++ src/main/java/com/bridgelabz/greetingappproject/GreetingAppProjectApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.greetingappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingAppProjectApplication.class, args);
+		System.out.println("Hello BridgeLabz");
+	}
+
+}
