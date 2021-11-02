ba687f212f947e0937cac7a1a54503733fc51e21
govind6498 <govindsingh6498@gmail.com>
2021-11-02 08:01:59 +0530

    MODIFIED:To responde greeting message bassed on user attributes

--- src/main/java/com/bridgelabz/greetingappproject/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingappproject/controller/GreetingController.java
@@ -2,38 +2,29 @@ package com.bridgelabz.greetingappproject.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.greetingappproject.model.Greeting;
 import com.bridgelabz.greetingappproject.model.User;
+import com.bridgelabz.greetingappproject.service.IGreetingService;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-	private static final String template = "Hello,%s!";
+	private static String template = "Hello ,%s!"
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping(value= {"","/","/home"})
-	public Greeting sysHello() {
-		return new Greeting(counter.incrementAndGet(),String.format(template,"World"));
-	}
-	@GetMapping("/query")
+	@Autowired
+	private IGreetingService greetingService;
+
+	@GetMapping(value= {"","/","/greeting"})
 	public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
-		return new Greeting(counter.incrementAndGet(),String.format(template,name));
-	}
-	@PostMapping("/post")
-	public Greeting greeting(@RequestBody User user) {
-		return new Greeting(counter.incrementAndGet(),String.format(template, user.getFirstName()+" "+user.getLastName()));
-	}
-	@PutMapping("put/{firstName}")
-	public Greeting greeting(@PathVariable String firstName,@RequestParam(value="lastName") String lastName){
-		return new Greeting(counter.incrementAndGet(),String.format(template,firstName+" "+lastName));
+		User user = new User();
+		user.setfirstName(name);
+		return greetingService.addGreeting(user);
 	}
 }
